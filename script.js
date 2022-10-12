1e42a1cf7f01c83a18770db4d11cfaf28d95631e
PriyaVivekRaj <109811104+PriyaVivekRaj@users.noreply.github.com>
2022-10-12 12:34:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function foo() {
+    var res = document.getElementById("first").value;
+    var res1 = document.getElementById("middle").value;
+    var res2 = document.getElementById("last").value; 
+    var res3 = document.getElementById("email").value;
+    var res4 = document.getElementById("psw").value; 
+    console.log(res,res1,res2,res3,res4);
+}
